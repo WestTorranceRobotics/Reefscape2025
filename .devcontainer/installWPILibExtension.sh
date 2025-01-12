1e1d6f2d04686407d56b85f09638d1e22ad29560
XLFrie <xlfrys@gmail.com>
2025-01-11 23:01:20 -0800

    Make better script to install vscode extension in codespaces (not tested for betas)

---
+++ .devcontainer/installWPILibExtension.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Use gradle dependency insight to get information on a wpilib package (contains version number). 
+WPILIB_VERSION=$(./gradlew dependencyInsight --dependency edu.wpi.first | grep -iEo "[0-9]{4}\.[0-9]\.[0-9]" | head -1)
+
+cd /opt 
+
+wget -O vscode-wpilib.vsix "https://github.com/wpilibsuite/vscode-wpilib/releases/latest/download/vscode-wpilib-${WPILIB_VERSION}.vsix"
