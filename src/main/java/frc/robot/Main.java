f4e9e5af6c0f2a99b96f1b45f4ead967d253062a
xlfrie <xlfrys@gmail.com>
2025-01-17 00:20:50 +0000

    Test

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -20,7 +20,7 @@ public final class Main {
    * If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
-
+    System.out.println("Hello 2!");
 
     System.out.println("Hello!");
   }
